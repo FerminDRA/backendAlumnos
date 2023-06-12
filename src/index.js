7780a0478d5608f4225f5bb29ed42a86c47abbfa
FerminDRA <zs19004879@estudiantes.uv.mx>
2023-06-11 19:08:26 -0600

    add cors, morgan

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express= require('express');
 const v1StudentRouter=require("./v1/routes/estudiantesRoutes");
 require('dotenv').config();
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app= express();
 const PORT= process.env.PORT || 3000;
+
+app.use(cors());
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use("/api/v1/estudiantes",v1StudentRouter);
